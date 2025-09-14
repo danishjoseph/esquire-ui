cbcf6f8e7ec9ca4f01d4e9fce475f0700a3b2cef
Danish Joseph <danishjoseph07@gmail.com>
2025-09-14 11:10:35 +0200

    feat: add ui button

---
+++ src/app/shared/components/ui/button.ts
@@ -0,0 +1,63 @@
+import { NgClass } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+import { SafeHtmlPipe } from '../../pipe/safe-html-pipe';
+
+@Component({
+  selector: 'app-button',
+  imports: [NgClass, SafeHtmlPipe],
+  template: `
+    <button
+      type="button"
+      [ngClass]="
+        'inline-flex items-center justify-center gap-2 rounded-lg transition ' +
+        className() +
+        ' ' +
+        sizeClasses +
+        ' ' +
+        variantClasses +
+        ' ' +
+        disabledClasses
+      "
+      [disabled]="disabled()"
+      (click)="onClick($event)"
+    >
+      @if (startIcon()) {
+        <span class="flex items-center" [innerHTML]="startIcon() | appSafeHtml"></span>
+      }
+      <ng-content></ng-content>
+      @if (endIcon()) {
+        <span class="flex items-center" [innerHTML]="endIcon() | appSafeHtml"></span>
+      }
+    </button>
+  `,
+})
+export class Button {
+  readonly size = input<'sm' | 'md'>('md');
+  readonly variant = input<'primary' | 'outline'>('primary');
+  readonly disabled = input(false);
+  readonly className = input('');
+  readonly startIcon = input('');
+  readonly endIcon = input('');
+
+  readonly btnClick = output<Event>();
+
+  get sizeClasses(): string {
+    return this.size() === 'sm' ? 'px-4 py-3 text-sm' : 'px-5 py-3.5 text-sm';
+  }
+
+  get variantClasses(): string {
+    return this.variant() === 'primary'
+      ? 'bg-brand-500 text-white shadow-theme-xs hover:bg-brand-600 disabled:bg-brand-300'
+      : 'bg-white text-gray-700 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:bg-gray-800 dark:text-gray-400 dark:ring-gray-700 dark:hover:bg-white/[0.03] dark:hover:text-gray-300';
+  }
+
+  get disabledClasses(): string {
+    return this.disabled() ? 'cursor-not-allowed opacity-50' : '';
+  }
+
+  onClick(event: Event) {
+    if (!this.disabled()) {
+      this.btnClick.emit(event);
+    }
+  }
+}
