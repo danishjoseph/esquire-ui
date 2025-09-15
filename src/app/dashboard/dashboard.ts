d2c7c44072f82c1041c802dacdf65d4c6ac86eef
Danish Joseph <danishjoseph07@gmail.com>
2025-09-16 00:37:13 +0200

    feat: add dashboard metrics

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { PageBreadcrumb } from '../shared/components/ui/page-breadcrumb';
-import { Maintenance } from '../maintenance';
+import { CustomerMetrics } from './customer-metrics';
+import { ProductMetrics } from "./product-metrics";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [PageBreadcrumb, Maintenance],
+  imports: [PageBreadcrumb, CustomerMetrics, ProductMetrics],
   template: `
     <app-page-breadcrumb pageTitle="Dashboard" />
-    <app-maintenance />
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6">
+      <app-customer-metrics />
+      <app-product-metrics/>
+    </div>
   `,
 })
 export class Dashboard {}
---
+++ src/app/dashboard/product-metrics.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Badge } from '../shared/components/ui/badge';
+import { SafeHtmlPipe } from '../shared/pipe/safe-html-pipe';
+
+@Component({
+  selector: 'app-product-metrics',
+  imports: [Badge, SafeHtmlPipe],
+  template: `
+    <div
+      class="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6"
+    >
+      <div
+        class="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800"
+        [innerHTML]="icons.boxIconLine | appSafeHtml"
+      ></div>
+      <div class="flex items-end justify-between mt-5">
+        <div>
+          <span class="text-sm text-gray-500 dark:text-gray-400">Products</span>
+          <h4 class="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">359</h4>
+        </div>
+        <app-badge color="error">
+          <span [innerHTML]="icons.arrowDownIcon | appSafeHtml"></span>
+          9.05%
+        </app-badge>
+      </div>
+    </div>
+  `,
+})
+export class ProductMetrics {
+  readonly icons = {
+    boxIconLine: `<svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="text-gray-800 size-6 dark:text-white/90"><path fill-rule="evenodd" clip-rule="evenodd" d="M11.665 3.75621C11.8762 3.65064 12.1247 3.65064 12.3358 3.75621L18.7807 6.97856L12.3358 10.2009C12.1247 10.3065 11.8762 10.3065 11.665 10.2009L5.22014 6.97856L11.665 3.75621ZM4.29297 8.19203V16.0946C4.29297 16.3787 4.45347 16.6384 4.70757 16.7654L11.25 20.0366V11.6513C11.1631 11.6205 11.0777 11.5843 10.9942 11.5426L4.29297 8.19203ZM12.75 20.037L19.2933 16.7654C19.5474 16.6384 19.7079 16.3787 19.7079 16.0946V8.19202L13.0066 11.5426C12.9229 11.5844 12.8372 11.6208 12.75 11.6516V20.037ZM13.0066 2.41456C12.3732 2.09786 11.6277 2.09786 10.9942 2.41456L4.03676 5.89319C3.27449 6.27432 2.79297 7.05342 2.79297 7.90566V16.0946C2.79297 16.9469 3.27448 17.726 4.03676 18.1071L10.9942 21.5857L11.3296 20.9149L10.9942 21.5857C11.6277 21.9024 12.3732 21.9024 13.0066 21.5857L19.9641 18.1071C20.7264 17.726 21.2079 16.9469 21.2079 16.0946V7.90566C21.2079 7.05342 20.7264 6.27432 19.9641 5.89319L13.0066 2.41456Z" fill="currentColor"></path></svg>`,
+    arrowDownIcon: `<svg class="fill-current" width="1em" height="1em" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.31462 10.3761C5.45194 10.5293 5.65136 10.6257 5.87329 10.6257C5.8736 10.6257 5.8739 10.6257 5.87421 10.6257C6.0663 10.6259 6.25845 10.5527 6.40505 10.4062L9.40514 7.4082C9.69814 7.11541 9.69831 6.64054 9.40552 6.34754C9.11273 6.05454 8.63785 6.05438 8.34486 6.34717L6.62329 8.06753L6.62329 1.875C6.62329 1.46079 6.28751 1.125 5.87329 1.125C5.45908 1.125 5.12329 1.46079 5.12329 1.875L5.12329 8.06422L3.40516 6.34719C3.11218 6.05439 2.6373 6.05454 2.3445 6.34752C2.0517 6.64051 2.05185 7.11538 2.34484 7.40818L5.31462 10.3761Z" fill=""></path></svg>`,
+    arrowUpIcon: `<svg class="fill-current" width="1em" height="1em" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M6.06462 1.62393C6.20193 1.47072 6.40135 1.37432 6.62329 1.37432C6.6236 1.37432 6.62391 1.37432 6.62422 1.37432C6.81631 1.37415 7.00845 1.44731 7.15505 1.5938L10.1551 4.5918C10.4481 4.88459 10.4483 5.35946 10.1555 5.65246C9.86273 5.94546 9.38785 5.94562 9.09486 5.65283L7.37329 3.93247L7.37329 10.125C7.37329 10.5392 7.03751 10.875 6.62329 10.875C6.20908 10.875 5.87329 10.5392 5.87329 10.125L5.87329 3.93578L4.15516 5.65281C3.86218 5.94561 3.3873 5.94546 3.0945 5.65248C2.8017 5.35949 2.80185 4.88462 3.09484 4.59182L6.06462 1.62393Z" fill=""></path></svg>`,
+  };
+}
