14902600d8d1061d981c80047f810bcf8bd79655
Danish Joseph <danishjoseph07@gmail.com>
2025-10-14 17:54:27 +0200

    feat: remove popper dep in dropdown component

--- src/app/shared/components/ui/dropdown.ts
+++ src/app/shared/components/ui/dropdown.ts
@@ -1,13 +1,12 @@
 import {
   AfterViewInit,
   Component,
-  DestroyRef,
+  computed,
   ElementRef,
-  inject,
+  input,
   signal,
   viewChild,
 } from '@angular/core';
-import { createPopper, Instance } from '@popperjs/core';
 
 @Component({
   selector: 'app-dropdown',
@@ -27,21 +26,19 @@ import { createPopper, Instance } from '@popperjs/core';
       </div>
 
       @if (isOpen()) {
-        <div #contentRef class="absolute right-0 z-10">
-          <div
-            class="p-2 bg-white border border-gray-200 rounded-2xl shadow-lg
-                   dark:border-gray-800 dark:bg-gray-900 w-40"
-            role="menu"
-            aria-orientation="vertical"
-            aria-labelledby="options-menu"
-            tabindex="0"
-            (click)="close()"
-            (keydown.enter)="toggle()"
-            (keydown.space)="toggle()"
-          >
-            <div class="space-y-1">
-              <ng-content select="[dropdown-content]"></ng-content>
-            </div>
+        <div
+          #contentRef
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="options-menu"
+          tabindex="0"
+          (click)="close()"
+          (keydown.enter)="toggle()"
+          (keydown.space)="toggle()"
+          [class]="dropdownClasses()"
+        >
+          <div class="space-y-1">
+            <ng-content select="[dropdown-content]"></ng-content>
           </div>
         </div>
       }
@@ -49,21 +46,16 @@ import { createPopper, Instance } from '@popperjs/core';
   `,
 })
 export class Dropdown implements AfterViewInit {
+  readonly className = input('');
   isOpen = signal(false);
 
   readonly buttonRef = viewChild.required<ElementRef<HTMLDivElement>>('buttonRef');
   readonly contentRef = viewChild<ElementRef<HTMLDivElement>>('contentRef'); // optional
 
-  private popperInstance: Instance | null = null;
-  private destroyRef = inject(DestroyRef);
-
-  constructor() {
-    this.destroyRef.onDestroy(() => {
-      document.removeEventListener('click', this.handleDocumentClick);
-      this.popperInstance?.destroy();
-      this.popperInstance = null;
-    });
-  }
+  readonly dropdownClasses = computed(
+    () =>
+      `p-2 bg-white border border-gray-200 rounded-2xl shadow-lg dark:border-gray-800 dark:bg-gray-900 + ${this.className()}`,
+  );
 
   ngAfterViewInit(): void {
     document.addEventListener('click', this.handleDocumentClick);
@@ -75,26 +67,6 @@ export class Dropdown implements AfterViewInit {
 
   toggle() {
     this.isOpen.update((v) => !v);
-
-    if (this.isOpen() && this.buttonRef() && this.contentRef()) {
-      // Init or update Popper
-      this.popperInstance = createPopper(
-        this.buttonRef().nativeElement,
-        this.contentRef()!.nativeElement,
-        {
-          placement: 'bottom-end',
-          modifiers: [
-            {
-              name: 'offset',
-              options: { offset: [0, 4] },
-            },
-            { name: 'preventOverflow', options: { boundary: 'viewport' } }, // <-- prevents clipping
-            { name: 'flip', options: { fallbackPlacements: ['bottom-start', 'top-end'] } }, // auto-flip
-          ],
-          // strategy: 'fixed',
-        },
-      );
-    }
   }
 
   private handleDocumentClick = (event: MouseEvent) => {
