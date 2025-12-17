85dae14f5c39e318c60dec2298cae229b4cca042
Danish Joseph <danishjoseph07@gmail.com>
2025-12-17 16:15:35 +0100

    fix: revert product warranty option picker

--- src/app/products/product-form.ts
+++ src/app/products/product-form.ts
@@ -90,12 +90,11 @@ export const productCategoryOptions: Option[] = [
             />
           </div>
           <div class="col-span-1 sm:col-span-2">
-            <app-select
+            <app-input
               id="product_warranty"
               label="Product Warranty"
               placeholder="Product Warranty"
               formControlName="product_warranty"
-              [options]="['Yes', 'No']"
             />
           </div>
         </div>
